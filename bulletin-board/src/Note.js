684db7eff1e80e32469a37b9ded2b8f41111b5e1
Ross Shtivelberg <RossTheDev@gmail.com>
2018-02-05 01:44:57 -0500

    Note comp for Bulletin

---
+++ bulletin-board/src/Note.js
@@ -0,0 +1,35 @@
+import React, { Component} from 'react'
+import FaPencil from 'react-icons/lib/fa/pencil'
+import FaTrash from 'react-icons/lib/fa/trash'
+
+class Note extends Component {
+  constructor(props){
+    super(props)
+    this.edit=this.edit.bind(this);
+    this.remove=this.remove.bind(this);
+  }
+
+  edit(){
+    alert("this is edited")
+  }
+
+  remove(){
+    alert("this is removed")
+  }
+
+
+
+  render() {
+      return (
+      <div className="note">
+        <p>Learn React </p>
+        <span>
+          <button onClick={this.edit} id="edit"> <FaPencil />< /button>
+          <button onClick={this.remove} id="remove"> <FaTrash /> </button>
+        </span>
+      </div>
+    )
+  }
+}
+
+export default Note
