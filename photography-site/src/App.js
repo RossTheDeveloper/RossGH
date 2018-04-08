5af1e3247de10b7f353e4964502381d27b1502af
Ross Shtivelberg <RossTheDev@gmail.com>
2018-04-07 21:35:18 -0400

    Css Modules

--- photography-site/src/App.js
+++ photography-site/src/App.js
@@ -4,66 +4,8 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div>
+    return (<h1>hi</h1>)
 
-
-
-
-
-
-
-
-      <div className="App App1">
-        <div className="div div1"> 1</div>
-          <div className="div div2"> 2</div>
-            <div className="div div3"> 3 </div>
-              <div className="div div4"> 4 </div>
-                <div className="div div5"> 5</div>
-
-
-
-
-
-
-      </div>
-
-
-
-............................................
-
-
-
-
-      <div className="App App2">
-      <div className="div div1"> 1 </div>
-        <div className="div div2"> today is sat </div>
-          <div className="div div3"> 3 </div>
-            <div className="div div4"> 4 </div>
-              <div className="div div4"> 5</div>
-                <div className="div div4"> 6 </div>
-                  <div className="div div4"> 7 </div>
-
-
-
-      </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      </div>
-    );
   }
 }
 
