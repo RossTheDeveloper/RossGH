bd2ad91a47d445c12e2fc310ac1a2cd50dd0b130
Ross Shtivelberg <RossTheDev@gmail.com>
2018-06-08 22:00:08 -0400

    cart total added by reduce

--- ecommerce/src/components/ProductBar.js
+++ ecommerce/src/components/ProductBar.js
@@ -43,10 +43,14 @@ console.log(this.state.cart)
 
               <div className="cpRight">
 
+              <div className="cartNP">
               <h4>{`${each.name} -${each.gender}`}</h4>
+              <h4>${each.price * each.qty}.00</h4>
+              </div>
+
               <h4>{`color -${each.color}`}</h4>
               <h4>{`size -${each.size}`}</h4>
-              <h4>{`QTY -${each.qty}`}</h4>
+              <h4>{`QTY -${each.qty} @ $${each.price}.00`}</h4>
 
               <div className="cartEdit">
                 <div className="removeProdCart">
@@ -113,6 +117,29 @@ console.log(this.state.cart)
         <div className="cs-right">
         <h2>Summary</h2>
         <hr />
+
+        <div className="Details">
+
+        <div className="subtotal">
+        {`SUBTOTAL $${this.state.cart.reduce((acc,each)=> {
+          const x =each.price*each.qty
+          return x + acc
+        },0)}`}
+        </div>
+
+        <div className="tax">
+        {`TAX`}
+        </div>
+
+        <div className="Total">
+        {'TOTAL'}
+        </div>
+
+        <div className="Total">
+        {`CHECKOUT`}
+        </div>
+
+        </div>
 
         </div>
 
